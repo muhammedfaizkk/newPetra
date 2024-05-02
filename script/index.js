a9c14b67dd3aca6f72284345f9dc9b26b0acc570
Muhammed faiz kk <fayizkk5@gmail.com>
2024-05-02 18:18:39 +0530

    added aboutuspage

--- script/index.js
+++ script/index.js
@@ -25,3 +25,24 @@ var mySwiper = new Swiper(".mySwiper", {
   },
 });
 
+
+var aboutUsswiper = new Swiper(".aboutUsswiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  autoplay: {
+      delay: 5000, 
+      disableOnInteraction: false,
+  },
+  pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+  },
+  navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+  },
+});
+
+
+
